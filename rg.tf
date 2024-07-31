4177feaaa88b7f2fd19baf40d1cc87d70cf2060b
chaitanya <chaitanyapanja987@gmail.com>
2024-07-30 22:39:36 -0500

    added 2 more rg

--- rg.tf
+++ rg.tf
@@ -10,3 +10,11 @@ resource "azurerm_resource_group" "azureaustin2" {
   name     = "azureaustin2"
   location = "eastus"
 }
+resource "azurerm_resource_group" "azureaustin3" {
+  name     = "azureaustin3"
+  location = "eastus"
+}
+resource "azurerm_resource_group" "azureaustin4" {
+  name     = "azureaustin4"
+  location = "eastus"
+}
