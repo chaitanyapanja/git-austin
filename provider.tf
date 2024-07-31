ecb1e6fc0918474120b84e6f4dee925efaf8652b
chaitanya <chaitanyapanja987@gmail.com>
2024-07-30 22:40:56 -0500

    added 2 more vnet

--- provider.tf
+++ provider.tf
@@ -7,3 +7,15 @@ resource "azurerm_virtual_network" "Vnet-1" {
   location            = azurerm_resource_group.azureaustin.location
   resource_group_name = azurerm_resource_group.azureaustin.name
 }
+resource "azurerm_virtual_network" "Vnet-2" {
+  name                = "Vnet-2"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.azureaustin.location
+  resource_group_name = azurerm_resource_group.azureaustin.name
+}
+resource "azurerm_virtual_network" "Vnet-3" {
+  name                = "Vnet-3"
+  address_space       = ["10.20.0.0/16"]
+  location            = azurerm_resource_group.azureaustin.location
+  resource_group_name = azurerm_resource_group.azureaustin.name
+}
